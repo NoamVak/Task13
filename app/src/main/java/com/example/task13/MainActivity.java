48b1f6304d402fd958f5043befa18681f03b9502
Noam <noamvak765@gmail.com>
2020-11-17 19:20:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/task13/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.task13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    ToggleButton kind;
+    EditText first,dif_mult;
+    double a1,d_m;
+    int choice;
+    String con;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        kind=(ToggleButton) findViewById(R.id.kind);
+        first=(EditText)findViewById(R.id.first);
+        dif_mult=(EditText)findViewById(R.id.dif_mult);
+    }
+
+
+
+
+    public void SubInitiate(View view) {
+        con=first.getText().toString();
+        if(con.equals("")){
+            Toast.makeText(this,"have to enter initiate",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            a1=Double.parseDouble(con);
+        }
+    }
+
+    public void SubDifMult(View view) {
+        con=dif_mult.getText().toString();
+        if(con.equals("")){
+            Toast.makeText(this,"have to enter initiate",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            d_m=Double.parseDouble(con);
+        }
+    }
+
+    public void Result(View view) {
+        if(dif_mult.getText().toString().equals("")||first.getText().toString().equals("")){
+            Toast.makeText(this,"nothing submitted",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Intent res=new Intent(this,SeriesInfo.class);
+            res.putExtra("choice",choice);
+            res.putExtra("a1",a1);
+            res.putExtra("d_m",d_m);
+            startActivity(res);
+        }
+    }
+    public void Register(View view) {
+        if(kind.isChecked()){
+            choice=1;
+        }
+        else{
+            choice=2;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/task13/SeriesInfo.java
@@ -0,0 +1,96 @@
+package com.example.task13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class SeriesInfo extends AppCompatActivity  implements View.OnCreateContextMenuListener , AdapterView.OnItemClickListener{
+    ListView series;
+    TextView info;
+    Intent res;
+    int choice,row;
+    double a1,d_m;
+    String [] seriesV=new String[20];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_series_info);
+
+        series=(ListView) findViewById(R.id.series);
+        info=(TextView)findViewById(R.id.info);
+
+        res=getIntent();
+        choice=res.getIntExtra("choice",-10);
+        a1=res.getDoubleExtra("a1",-99999);
+        d_m=res.getDoubleExtra("d_m",-999);
+
+        series.setOnItemClickListener(this);
+        series.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, seriesV);
+        series.setAdapter(adp);
+
+        FillArray();
+        seriesV[0]=String.valueOf(a1);
+
+        series.setOnCreateContextMenuListener(this);
+    }
+
+    public void FillArray(){
+        if(choice==1){
+            for (int i=1;i<20;i++){
+                seriesV[i]=String.valueOf(a1*(Math.pow(d_m,i)));
+            }
+        }
+        else if(choice==2){
+            for (int i=1;i<20;i++){
+                seriesV[i]=String.valueOf(a1+d_m*(i));
+            }
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("Series Info");
+        menu.add("n");
+        menu.add("Sn");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        String oper=item.getTitle().toString();
+        if(oper.equals("n")){
+            info.setText(""+row);
+            return true;
+        }
+        else if(oper.equals("Sn")){
+            if(choice==1){
+                double sum=Math.pow(d_m,row+1);
+                info.setText(String.valueOf((a1*sum-a1)/(d_m-1)));
+                return true;
+            }
+            else{
+                double sum=2*a1+d_m*(row+1)-d_m;
+                info.setText(String.valueOf(((row+1)*sum)/2));
+                return true;
+            }
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        row=i;
+
+    }
+}
